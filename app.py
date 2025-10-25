0a177d474efa8dcd00e15d8cf5b2ad798db1d952
mumbly6 <mumblykilonzo@gmail.com>
2025-10-25 14:25:36 +0300

    Add database setup in app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+#app/databse.py
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///./orders.db"
+
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+metadata = MetaData()
